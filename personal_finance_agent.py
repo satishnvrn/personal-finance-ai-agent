62b71f3f2c6adb69334dcc9166939e0a80808f34
satishnvrn <nvrn.satish@gmail.com>
2024-12-29 23:27:22 +0530

    Added agent for web search and finance search

---
+++ personal_finance_agent.py
@@ -0,0 +1,50 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# web search agent
+web_search_agent = Agent(
+    name="Web search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+
+# Financial agent
+finance_agent = Agent(
+    name="Finance AI agent",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[
+        YFinanceTools(
+            stock_price=True,
+            analyst_recommendations=True,
+            stock_fundamentals=True,
+            company_news=True,
+        )
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent = Agent(
+    team=[web_search_agent, finance_agent],
+    model=Groq(id="llama-3.3-70b-versatile"),
+    instructions=["Always include sources", "Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response(
+    "Summarize analyst recommendations and share the latest news for Apple Inc. (AAPL)."
+)
